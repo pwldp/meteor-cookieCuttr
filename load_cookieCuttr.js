549c8b4c1b28d494e783e7863e66c3123d369673
pwldp <pawlakdp@gmail.com>
2013-12-13 15:51:26 +0100

    Update load_cookieCuttr.js

--- load_cookieCuttr.js
+++ load_cookieCuttr.js
@@ -1,5 +1,10 @@
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    $.cookieCuttr();
+	  if (Meteor.settings && Meteor.settings.public !== undefined && Meteor.settings.public.cookieCuttr !== undefined){
+	    $.cookieCuttr( Meteor.settings.public.cookieCuttr );
+	  } else {
+	    console.log("You can customize cookieCuttr by adding public.cookieCuttr in settings file.");
+	    $.cookieCuttr();
+	  };
   });
 }
